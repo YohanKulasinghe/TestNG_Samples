ea0123c5c7de39bdd62f8d837f3e53e8ef99fd0e
yohan kulasinghe <yohankulasinghe@gmail.com>
2020-06-29 08:45:03 +0530

    Use data providers

--- src/test/java/simple/calculator/test/CalcLogicTest.java
+++ src/test/java/simple/calculator/test/CalcLogicTest.java
@@ -2,6 +2,7 @@ package simple.calculator.test;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import simple.calculator.CalcLogic;
 
@@ -14,8 +15,33 @@ public class CalcLogicTest {
         calc = new CalcLogic();
     }
 
+    @Test(dataProvider = "getNum")
+    public void testSum(int input1, int input2, int expectedResults) {
+        Assert.assertEquals(calc.sum(input1, input2), expectedResults, "Failed to add two integers");
+    }
+
+    /*@Test
+    public void testSum2() {
+        Assert.assertEquals(calc.sum(-100, -50), -150, "Failed to add two integers");
+    }
+
     @Test
-    public void testSum() {
-        Assert.assertEquals(calc.sum(100, 50), 150, "Failed to add two integers");
+    public void testSum3() {
+        Assert.assertEquals(calc.sum(100, -50), 50, "Failed to add two integers");
+    }
+
+    @Test
+    public void testSum4() {
+        Assert.assertEquals(calc.sum(-100, 50), -50, "Failed to add two integers");
+    }*/
+
+    @DataProvider
+    public Object[][] getNum(){
+        return new Object[][]{
+            {100, 50, 150},
+            {-100, -50, -150},
+            {100, -50, 50},
+            {-100, 50, -50}
+        };
     }
 }
